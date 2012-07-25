41022aac42a8815ffa2f7882634bfdec2154a4a3
David Broadfoot <david@thinkcreative.com>
2012-07-25 10:29:17 -0400

    better persistent variable handling

--- operators/pagedataoperator.php
+++ operators/pagedataoperator.php
@@ -313,7 +313,12 @@ class PageDataOperator
 			$PersistentVariable[$key] = $Value;
 		}
 		// storing the value internally as well in case this is not a view that supports persistent_variable (pagedata will look for it)
+		eZDebug::writeDebug($PersistentVariable);
 		self::$PersistentVariable = $PersistentVariable;
+		// set the template persistent_variable to the new persistent_variable value
+		if($tpl->hasVariable('persistent_variable')){
+			$tpl->setVariable('persistent_variable', self::$PersistentVariable);
+		}
 	}
 
 	static private function getInfoboxPosition($node, $identifier){
